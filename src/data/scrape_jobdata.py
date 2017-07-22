737510069c2d8605c977b2c42a2a0539cb45c348
Rick Moton <dmoton3@gatech.edu>
2017-07-22 18:24:17 -0400

    add basic scrape logic

--- src/data/scrape_jobdata.py
+++ src/data/scrape_jobdata.py
@@ -9,7 +9,15 @@ def get_html(url):
     except urllib3.exceptions.NewConnectionError:
         print('Connection failed.')
         return None
-    return request
+    return request.data
 
-def scrape(url):
-    pass
+def scrape_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    # job_content = soup.find(id="job-content")
+    # job_header = soup.find(id="job-header
+    job_summary = soup.find(id="job_summary")
+    for p in job_summary.find_all('p'):
+        print(p)
+        print("--------------------")
+    return job_summary
